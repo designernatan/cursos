0d75a9ca038dfd70ff434da46973fc0e91b54ca9
Natan Souza <ntnleao@gmail.com>
2016-07-13 12:30:16 -0300

    Ex 1.12 - Iniciando a aplicacao

---
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,10 @@
+angular
+  .module('alurapic')
+  .controller('FotosController', function($scope) {
+
+    $scope.foto = {
+      titulo : 'aviao legal'
+      ,url : 'http://lorempixel.com/image_output/transport-q-c-312-269-2.jpg'
+    };
+  
+  });
