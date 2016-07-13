7a2c69456075e0b2a22ec4f456fc18e708dab77f
Natan Souza <ntnleao@gmail.com>
2016-07-13 17:50:50 -0300

    Ex 5.6 - Transformando a Alurapic numa SPA

--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,1 +1,17 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+  .config(function($routeProvider, $locationProvider) {
+  
+    $locationProvider.html5Mode(true);
+  
+    $routeProvider
+      .when('/fotos', {
+        templateUrl: 'partials/principal.html',
+        controller: 'FotosController'
+      })
+      .otherwise({redirectTo: '/fotos'})
+      .when('/fotos/new', {
+        templateUrl: 'partials/foto.html'    
+      });
+    
+    
+  });
